79112fbe736a5acfd413065f3bc481821a220572
haruki hayashi <haruki@harukinoMacBook-Pro.local>
2022-11-14 16:18:49 +0900

    add_Nlist

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import os
+from typing import Callable
 
 
 def hayashi(x,y):
     return x+y
 
+
 def kaneki(x,y):
     return x-y
+
+
+class Nlist(list):
+    def map(self, func:Callable):
+        return map(lambda x: func(x), self)
